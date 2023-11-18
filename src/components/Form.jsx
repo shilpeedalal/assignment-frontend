8df85aab46b733cce00e0a09cbb41ad010d7b627
Shilpee <shilpeedalal937@gmail.com>
2023-11-18 12:34:21 +0530

    initial commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,14 @@
 import { useState } from "react";
+import axios from "axios";
 
 const initialValues = {
-    jobTitle: "",
-    email: "",
-    username: "",
-    applicationDeadlineDate: "",
-    specialisms: "",
-    // jobtype: "",
-    // offeredSalary: "",
-    // careerLevel: "",
-    // experience: "",
-    // gender: "",
-    // industry: "",
-    // qualification: "",
-    // country: "",
-    // city: "",
-    // completeAddress: "",
-    // findOnMap: "",
-    // latitude: "",
-    // longitude: "",
-    jobDescription: "",
-  };
+  jobTitle: "",
+  email: "",
+  username: "",
+  applicationDeadlineDate: "",
+  specialisms: "",
+  jobDescription: "",
+};
 
 const Form = () => {
   const [data, setData] = useState(initialValues);
@@ -34,129 +22,135 @@ const Form = () => {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/post', {
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-    const result = await response.json();
-    console.log(result);
+
+    const response = await axios.post("http://localhost:8000/post", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if(response.status===200) {
+      alert("Success")
+    }
   };
 
-
   return (
-<div className="flex items-center justify-center p-12">
-  <div className="mx-auto w-full max-w-[550px]">
-    <form method="POST" onSubmit={handleFormSubmit}>
-      <div className="mb-5">
-        <label
-          htmlFor="jobTitle"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Job Title
-        </label>
-        <input
-          type="text"
-          name="jobTitle"
-          id="jobTitle"
-          value={data.jobTitle}
-          onChange={handleFormData}
-          placeholder="jobTitle"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Email Address
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={data.email}
-          onChange={handleFormData}
-          placeholder="example@domain.com"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="username"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={data.username}
-          onChange={handleFormData}
-          placeholder="Enter your Username"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="flex flex-col mb-4">
-      <label
-          htmlFor="applicationDeadlineDate"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Application Deadline Date
-        </label>
-        <input className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" 
-        type="date" 
-        name="applicationDeadlineDate" 
-        id="applicationDeadlineDate"
-        value={data.applicationDeadlineDate}
-        onChange={handleFormData}
-        />
-      </div>
+    <div className="flex items-center justify-center p-12">
+      <div className="mx-auto w-full max-w-[550px]">
+        <form method="POST" onSubmit={handleFormSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="jobTitle"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Job Title
+            </label>
+            <input
+              type="text"
+              name="jobTitle"
+              id="jobTitle"
+              value={data.jobTitle}
+              onChange={handleFormData}
+              placeholder="jobTitle"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={data.email}
+              onChange={handleFormData}
+              placeholder="example@domain.com"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="username"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={data.username}
+              onChange={handleFormData}
+              placeholder="Enter your Username"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label
+              htmlFor="applicationDeadlineDate"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Application Deadline Date
+            </label>
+            <input
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              type="date"
+              name="applicationDeadlineDate"
+              id="applicationDeadlineDate"
+              value={data.applicationDeadlineDate}
+              onChange={handleFormData}
+            />
+          </div>
 
+          <div className="flex flex-col mb-4">
+            <label
+              htmlFor="specialisms"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Specialisms
+            </label>
+            <select
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              id="specialisms"
+              value={data.specialisms}
+              name="specialisms"
+              onChange={handleFormData}
+            >
+              <option>Retail</option>
+              <option>Banking</option>
+              <option>Management</option>
+              <option>Human Resources</option>
+            </select>
+          </div>
 
-      <div className="flex flex-col mb-4">
-      <label
-          htmlFor="specialisms"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Specialisms
-        </label>
-                <select className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" id="specialisms" value={data.specialisms} name="specialisms" onChange={handleFormData}>
-                    <option>Retail</option>
-                    <option>Banking</option>
-                    <option>Management</option>
-                    <option>Human Resources</option>
-                </select>
-            </div>
-
-      <div className="mb-5">
-        <label
-          htmlFor="jobDescription"
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Job Description
-        </label>
-        <textarea
-          rows="4"
-          name="jobDescription"
-          id="jobDescription"
-          value={data.jobDescription}
-          onChange={handleFormData}
-          placeholder="Type your description"
-          className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        ></textarea>
+          <div className="mb-5">
+            <label
+              htmlFor="jobDescription"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Job Description
+            </label>
+            <textarea
+              rows="4"
+              name="jobDescription"
+              id="jobDescription"
+              value={data.jobDescription}
+              onChange={handleFormData}
+              placeholder="Type your description"
+              className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            ></textarea>
+          </div>
+          <div>
+            <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <button
-          className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-  )
-}
-export default Form
+    </div>
+  );
+};
+export default Form;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
 import { useState, useEffect } from "react"
+import axios from "axios"
+import Card from "../components/Card"
 
 const Home = () => {
+const [applications, setApplications] = useState([])
 
+  useEffect(() => {
 
-  // useEffect(() => {
+    const fetchApplications = async () => {
+      const response = await axios.get('http://localhost:5000/');
+      console.log(response.data[0]);
+      if(response.status===200) {
+        setApplications(response.data)
+      }
+    }
 
-  //   const fetchApplications = async () => {
+    fetchApplications();
 
-  //   }
-
-  //   fetchApplications();
-
-  // }, [])
+  }, [])
 
 
   return (
-    <div>Home</div>
+    <div className="p-8">
+    <h2 className="text-4xl font-bold text-center mb-4">Applications</h2>
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto max-w-screen-2xl gap-4">
+      {
+        applications?.map(application => <Card key={application._id} data={application} />)
+      }
+    </div>
+    </div>
   )
 }
 export default Home
